4482bc109ecbcb075d814f110fd870ed066aad20
AntOne777 <sedeo25@gmail.com>
2026-07-11 00:18:34 +0300

    Update fetch_bing.py

--- fetch_bing.py
+++ fetch_bing.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import os
-import base64
 
 MARKETS = ['en-US', 'zh-CN', 'ja-JP', 'de-DE', 'fr-FR', 'ru-RU']
 
@@ -29,26 +28,13 @@ def fetch_wallpapers():
                     key = f"{date}_{mkt}"
                     
                     if key not in db:
-                        # Получаем чистое имя файла из Microsoft (например, VictoriaBeach)
-                        urlbase = img.get('urlbase', '')
-                        img_name = urlbase.split('?id=OHR.')[-1].split('_')[0] if '?id=OHR.' in urlbase else 'Wallpaper'
-                        
-                        # Формируем дату для пути китайского сервера (ггггмм)
-                        folder_date = f"{date[:4]}{date[4:6]}"
-                        
-                        # Собираем прямую ссылку на исходник, которую использует китайский сайт
-                        target_url = f"https://img.nanxiongnandi.com/{folder_date}/{img_name}.jpg"
-                        
-                        # Кодируем целевую ссылку в Base64 для imgproxy
-                        encoded_url = base64.urlsafe_b64encode(target_url.encode('utf-8')).decode('utf-8').rstrip('=')
-                        
-                        # Собираем финальную тяжелую ссылку на 10 МБ в максимальном качестве
-                        proxy_url = f"https://imgproxy.nanxiongnandi.com/2cHFkUCWFcv3j2aRgfTrrKkRYeLGsKfw3EPwDktoC0E/w:3840/q:100/att:1/{encoded_url}.jpg"
+                        # Собираем прямую официальную ссылку Microsoft на UHD 4K качество
+                        base_url = "https://www.bing.com" + img['urlbase'] + "_UHD.jpg"
                         
                         db[key] = {
                             "date": f"{date[:4]}-{date[4:6]}-{date[6:]}",
                             "market": mkt,
-                            "url": proxy_url,
+                            "url": base_url,
                             "copyright": img.get('copyright', '')
                         }
                         updated = True
@@ -58,7 +44,7 @@ def fetch_wallpapers():
     if updated or not db:
         with open('data.json', 'w', encoding='utf-8') as f:
             json.dump(db, f, ensure_ascii=False, indent=4)
-        print("База успешно переведена на супер-качество!")
+        print("База успешно возвращена к официальным 4K-ссылкам!")
 
 if __name__ == "__main__":
     fetch_wallpapers()
